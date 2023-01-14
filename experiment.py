893a622fa618f57359d39a4ed77a4a2c82e26c0b
Diego Jurado <diego.jurado@ieee.org>
2023-01-14 14:59:45 -0500

    Wrote changes; Something's still wrong

--- experiment.py
+++ experiment.py
@@ -12,9 +12,45 @@ from sklearn import svm
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.model_selection import train_test_split
-import warnings; warnings.filterwarnings("ignore")
+import warnings;
+
+warnings.filterwarnings("ignore")
 
 ''' ------ HELPER METHODS ------ '''
+
+'''
+Method:
+    1. Import Data
+    
+    Original: 
+    2. Split Original Data
+        a. Original Train - 80%
+        b. Original Test  - 20%
+    3. Train Original Model
+    4. Test Original Model
+    5. Report Results - Pass on false negative and false positive ratio
+    
+    Random Re-Annotation Sampling:
+    6. Split Random Re-Annotation Data
+        a. Random Train - 80%
+        b. Random Test  - 20%
+    7. Train Random Model
+    8. Test Random Model
+    9. Report Results
+    
+    Intelligent Re-Annotation Sampling:
+    10. Split Intelligent Re-Annotation Data
+        a. Intelligent Train - 80%
+        b. Intelligent Test  - 20%
+    11. Train Intelligent Model
+    12. Test Intelligent Model
+    13. Report Results
+    
+    14. Testing the two new methods against the test split for the original test data
+    
+    
+'''
+
 
 # Data Processing
 # params: String file
@@ -22,7 +58,7 @@ import warnings; warnings.filterwarnings("ignore")
 def import_data(file):
     df = pd.read_csv(file).drop_duplicates()
     # randomly sample 3000 hate speech
-    hate = df.query('Class == "hate"').sample(n=3000).copy() 
+    hate = df.query('Class == "hate"').sample(n=3000).copy()
     # randomly sample 3000 non-hate speech
     non_hate = df.query('Class == "none"').sample(n=3000).copy()
     # merge hate and non-hate
@@ -30,19 +66,21 @@ def import_data(file):
 
     return df
 
+
 # Data Set Information Reporting
 # params: DataFrame df
 def data_stat(df):
     length = len(df)
     h_length = len(df.query('Class == "hate"'))
     n_length = len(df.query('Class == "none"'))
-    print("\nNumber of samples:", length,"\nNumber of Hate Samples:", h_length, "\nNumber of non-Hate Samples:", n_length)
+    print("\nNumber of samples:", length, "\nNumber of Hate Samples:", h_length, "\nNumber of non-Hate Samples:",
+          n_length)
+
 
 # Randomized Re-Annotation Sampling
 # params: Dataframe df, float fn, float fp
 # return: DataFrame rdf
 def rand_annot_samp(df, fn, fp):
-
     rdf = df.copy()
 
     fn_tweets = df.query('Class == "hate"').sample(frac=fn).Tweets.tolist()
@@ -57,11 +95,11 @@ def rand_annot_samp(df, fn, fp):
 
     return rdf
 
+
 # Intelligent Re-Annotation Sampling
 # params: Dataframe df, String[] tweets, float fn, float fp
 # return: DataFrame idf
 def inte_annot_samp(df, tweets, fn, fp):
-
     W_NORM = np.linalg.norm(clf.coef_)
     dist = []
     idf = df.copy()
@@ -70,14 +108,14 @@ def inte_annot_samp(df, tweets, fn, fp):
         x = vectorizer.fit_transform([t])
         x = x.toarray()
 
-        zeros = [[0] for l in range(0,5000 - len(x[0]))]
-        
-        x = np.append(x,zeros)
-        x = np.reshape(x,(1,-1))
+        zeros = [[0] for l in range(0, 5000 - len(x[0]))]
+
+        x = np.append(x, zeros)
+        x = np.reshape(x, (1, -1))
 
         dist.append((clf.decision_function(x) / W_NORM)[0])
 
-    dict = {"Tweets" : tweets, "Distance" : dist}
+    dict = {"Tweets": tweets, "Distance": dist}
 
     new_df = pd.DataFrame.from_dict(dict)
 
@@ -98,21 +136,22 @@ def inte_annot_samp(df, tweets, fn, fp):
         elif (row["Class"] == "none" and n_to_p != int(fp * 3000)):
             idf.loc[idf["Tweets"] == tweet, "Class"] = "hate"
             n_to_p += 1
-    
+
         if n_to_p == int(fp * 3000) and p_to_n == int(fn * 3000):
             break
-            
+
     print("\nTotal Intelligent Re-annotations:", int(fp * 3000) + int(fn * 3000))
 
     return idf
+
 
 ''' ------ MAIN METHOD ------ '''
 
 results = {}
 
-trials = 16
+trials = 5
 
-for trial in range(1,trials):
+for trial in range(1, trials):
     print("\n------- Trial:", trial, "------")
     # ------ Baseline ------ #
 
@@ -122,31 +161,35 @@ for trial in range(1,trials):
     # Data Split
     X_train, X_test, Y_train, Y_test = train_test_split(df.Tweets, df.Class, test_size=0.2)
 
-    vectorizer = CountVectorizer(analyzer = "word",tokenizer = None,preprocessor = None,stop_words = None,max_features = 5000)
+    original_y_test = Y_test.copy()
+
+    vectorizer = CountVectorizer(analyzer="word", tokenizer=None, preprocessor=None, stop_words=None, max_features=5000)
 
     train_data_features = vectorizer.fit_transform(X_train)
     train_data_features = train_data_features.toarray()
-    
+
     test_data_features = vectorizer.transform(X_test)
     test_data_features = test_data_features.toarray()
 
+    original_test_features = test_data_features.copy()  # for later use
+
     # Classifier
-    clf=svm.SVC(kernel='linear', C=1.0)
+    clf = svm.SVC(kernel='linear', C=1.0)
 
-    print ("\nTraining Baseline SVM")
-    clf.fit(train_data_features,Y_train)
+    print("\nTraining Baseline SVM")
+    clf.fit(train_data_features, Y_train)
 
-    print ("Testing Baseline SVM")
-    predicted=clf.predict(test_data_features)
+    print("Testing Baseline SVM")
+    predicted = clf.predict(test_data_features)
 
     # Appending Results
     p, r, f, non = precision_recall_fscore_support(Y_test, predicted, average="weighted")
     b_key = "Baseline-" + str(trial)
-    results[b_key] = {"Precision" : p, "Recall" : r, "F-Score" : f}
+    results[b_key] = {"Precision": p, "Recall": r, "F-Score": f}
 
     # ------ Tweet Processing ------ #
 
-    t_dict = {"Tweets" : X_test, "Actual" : Y_test, "Predicted" : predicted}
+    t_dict = {"Tweets": X_test, "Actual": Y_test, "Predicted": predicted}
     df2 = pd.DataFrame(t_dict).reset_index()
 
     fn_tweets = df2.query('Actual == "hate" and Actual != Predicted').Tweets.copy()
@@ -156,7 +199,7 @@ for trial in range(1,trials):
     fn = len(fn_tweets) / len(df2)
     fp = len(fp_tweets) / len(df2)
 
-    tweets = fn_tweets.append(fp_tweets) 
+    tweets = fn_tweets.append(fp_tweets)
     tweets = tweets.tolist()
 
     # ------ Random Sampling ------ #
@@ -177,7 +220,7 @@ for trial in range(1,trials):
     r_clf = svm.SVC(kernel='linear', C=1.0)
 
     print("\nTraining Random Sampling SVM")
-    r_clf.fit(train_data_features,Y_train)
+    r_clf.fit(train_data_features, Y_train)
 
     print("Testing Random Sampling SVM")
     r_predicted = r_clf.predict(test_data_features)
@@ -185,13 +228,13 @@ for trial in range(1,trials):
     # Appending Results
     p, r, f, non = precision_recall_fscore_support(Y_test, r_predicted, average="weighted")
     r_key = "Randomized-" + str(trial)
-    results[r_key] = {"Precision" : p, "Recall" : r, "F-Score" : f}
+    results[r_key] = {"Precision": p, "Recall": r, "F-Score": f}
 
     # ------ Intelligent Sampling ------ #
 
     # Data Processing
     idf = inte_annot_samp(df, tweets, fn, fp)
-    
+
     # Data Split
     X_train, X_test, Y_train, Y_test = train_test_split(idf.Tweets, idf.Class, test_size=0.2)
 
@@ -205,7 +248,7 @@ for trial in range(1,trials):
     i_clf = svm.SVC(kernel='linear', C=1.0)
 
     print("\nTraining Intelligent Sampling SVM")
-    i_clf.fit(train_data_features,Y_train)
+    i_clf.fit(train_data_features, Y_train)
 
     print("Testing Intelligent Sampling SVM\n")
     i_predicted = i_clf.predict(test_data_features)
@@ -213,7 +256,25 @@ for trial in range(1,trials):
     # Appending results
     p, r, f, non = precision_recall_fscore_support(Y_test, i_predicted, average="weighted")
     i_key = "Intelligent-" + str(trial)
-    results[i_key] = {"Precision" : p, "Recall" : r, "F-Score" : f}
+    results[i_key] = {"Precision": p, "Recall": r, "F-Score": f}
+
+    # ------ Testing against original test set ------#
+
+    print("\nTesting Random Sampling SVM against original test set")
+    predicted = r_clf.predict(original_test_features)
+
+    # Appending Results
+    p, r, f, non = precision_recall_fscore_support(original_y_test, predicted, average="weighted")
+    r_key = "Randomized-" + str(trial) + "-original"
+    results[r_key] = {"Precision": p, "Recall": r, "F-Score": f}
+
+    print("\nTesting Intelligent Sampling SVM against original test set")
+    predicted = i_clf.predict(original_test_features)
+
+    # Appending results
+    p, r, f, non = precision_recall_fscore_support(original_y_test, predicted, average="weighted")
+    i_key = "Intelligent-" + str(trial) + "-original"
+    results[i_key] = {"Precision": p, "Recall": r, "F-Score": f}
 
 # ------ End of Trials ------ #
 
--- interpreting.py
+++ interpreting.py
@@ -6,6 +6,8 @@ import csv
 b_change = {}
 r_change = {}
 i_change = {}
+rvo_change = {}
+ivo_change = {}
 
 df = pd.read_csv("results.csv")
 
@@ -13,22 +15,32 @@ for row in df.iterrows():
     # metric strings
     m = row[1][0]
     m_change = m + "-Change"
+    print(row)
+    print(m)
+    print(m_change)
 
     # set of each result of each trial for a specific metric
-    b = [float(row[1][t]) for t in range(1,46,3)]
-    r = [float(row[1][t]) for t in range(2,46,3)]
-    i = [float(row[1][t]) for t in range(3,46,3)]
-    
-    # mean average performance 
+    b = [float(row[1][t]) for t in range(1,20,5)]
+    r = [float(row[1][t]) for t in range(2,20,5)]
+    i = [float(row[1][t]) for t in range(3,20,5)]
+    rvo = [float(row[1][t]) for t in range(4,20,5)]
+    ivo = [float(row[1][t]) for t in range(5,20,5)]
+
+    # mean average performance
     b_change[m] = sum(b) / len(b)
     r_change[m] = sum(r) / len(r)
     i_change[m] = sum(i) / len(i)
+    rvo_change[m] = sum(rvo) / len(rvo)
+    ivo_change[m] = sum(ivo) / len(ivo)
 
     # mean average performance shift
     b_change[m_change] = (sum(b) / len(b)) - (sum(b) / len(b))
     r_change[m_change] = (sum(r) / len(r)) - (sum(b) / len(b))
     i_change[m_change] = (sum(i) / len(i)) - (sum(b) / len(b))
+    rvo_change[m_change] = (sum(rvo) / len(rvo)) - (sum(b) / len(b))
+    ivo_change[m_change] = (sum(ivo) / len(ivo)) - (sum(b) / len(b))
 
-res = {"Baseline" : b_change, "Randomized" : r_change, "Intelligent" : i_change}
+res = {"Baseline" : b_change, "Randomized" : r_change, "Intelligent" : i_change, "Randomized-Original" : rvo_change, "Intelligent-Original" : ivo_change}
 df = pd.DataFrame.from_dict(res)
+df.to_csv("fixed.csv")
 print(df)
